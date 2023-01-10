be32c567262aeeb2488bcaf2ac842a05711a1937
karlo <karlsgit1126@gmail.com>
2023-01-10 23:52:56 +0800

    added DOM event and changed the padding of the header on scroll down

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const header = document.getElementById("header");
+
+window.onscroll = (e) => {
+  if (scrollY >= 100) {
+    header.classList.remove("header-on-top");
+    header.classList.add("header-on-scroll");
+  } else if (scrollY < 100) {
+    header.classList.remove("header-on-scroll");
+    header.classList.add("header-on-top");
+  }
+};
