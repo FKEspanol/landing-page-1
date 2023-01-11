e62e60d47b06b279af16f11d396330d4e63eb1fc
karlo <karlsgit1126@gmail.com>
2023-01-11 21:40:42 +0800

    modified the hero section, added responsive header, menu and menu button for smaller screen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 const header = document.getElementById("header");
+const menuBtn = document.getElementById("show-menu-btn");
+const dropDownMenu = document.getElementById("drop-down-menu");
 
 window.onscroll = (e) => {
   if (scrollY >= 100) {
     header.classList.remove("header-on-top");
-    header.classList.add("header-on-scroll");
+    header.classList.add("header-on-scroll", "show-header-bg");
   } else if (scrollY < 100) {
-    header.classList.remove("header-on-scroll");
+    header.classList.remove("header-on-scroll", "show-header-bg");
     header.classList.add("header-on-top");
   }
 };
+
+let menuIsClosed = true;
+menuBtn.addEventListener("click", (e) => {
+  if (menuIsClosed) {
+    dropDownMenu.style.display = "block";
+    dropDownMenu.style.height = "auto";
+    menuIsClosed = false;
+  } else if (!menuIsClosed) {
+    dropDownMenu.style.display = "none";
+    dropDownMenu.style.height = "0px";
+    menuIsClosed = true;
+  }
+});
